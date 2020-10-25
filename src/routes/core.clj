e582abc2c45b76d17cba225fb3a3938a5463fe2e
João Bittencourt <joaoasbittencourt@gmail.com>
2020-10-24 22:50:19 -0300

    starting to create the backend

---
+++ src/routes/core.clj
@@ -0,0 +1,15 @@
+(ns routes.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [reitit.ring :as ring]))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/api" {:get (fn [req]
+                     {:status 200
+                      :body "ok"})}]])))
+
+
+(defn -main []
+  (println "Server Started")
+  (run-server app {:port 4000}))
